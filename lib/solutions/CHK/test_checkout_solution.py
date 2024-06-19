54503d56789d101f5103f1164998913563e84035
sbx_user1051 <sbx_user1051@169.254.88.21>
2024-06-19 17:49:22 +0000

    Wed Jun 19 17:49:22 UTC 2024

---
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -0,0 +1,25 @@
+from checkout_solution import checkout
+import unittest
+
+
+class TestCheckout(unittest.TestCase):
+
+    def setUp(self):
+        self.test_cases = [
+            ("A", 50),
+            ("B", 30),
+            ("C", 20),
+            ("D", 15),
+            ("AAA", 130),
+            ("BB", 45)
+        ]
+
+    def test_checkout(self):
+        for param, expected in self.test_cases:
+            with self.subTest(param=param, expected=expected):
+                result = checkout(param)
+                self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
