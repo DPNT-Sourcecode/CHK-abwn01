926173dca4658bd425520ea8ce309d1cf33a236f
sbx_user1051 <sbx_user1051@169.254.88.21>
2024-06-19 19:00:44 +0000

    Wed Jun 19 19:00:44 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -35,17 +35,30 @@ def checkout(skus: str) -> int:
         items_sorted.sort(key=lambda x: prices[x], reverse=True)
         valid_item_count = sum(cart.get(k, 0) for k in items_sorted)
 
-        if valid_item_count >= required:
-            offer_count = valid_item_count // required
-            total += offer_count * price
-            items_in_offer = offer_count * required
+    for items, required, price in group_offer:
+        items_sorted = list(items)
+        items_sorted.sort(key=lambda x: prices[x], reverse=True)
+        valid_item_count = sum(cart.get(k, 0) for k in items_sorted)
+        while valid_item_count >= required:
+            total += price
+            items_in_offer = required
+            for item in items_sorted:
+                while cart[item] > 0 and items_in_offer > 0:
+                    cart[item] -= 1
+                    items_in_offer -= 1
+                    valid_item_count -= 1
 
-            while items_in_offer > 0:
-                for item in items_sorted:
-                    if cart.get(item, 0) > 0:
-                        cart[item] -= 1
-                        items_in_offer -= 1
-                        break
+        # if valid_item_count >= required:
+        #     offer_count = valid_item_count // required
+        #     total += offer_count * price
+        #     items_in_offer = offer_count * required
+
+        #     while items_in_offer > 0:
+        #         for item in items_sorted:
+        #             if cart.get(item, 0) > 0:
+        #                 cart[item] -= 1
+        #                 items_in_offer -= 1
+        #                 break
 
     # apply multi offers
     for item, count in cart.items():
@@ -58,6 +71,7 @@ def checkout(skus: str) -> int:
         total += count * prices[item]
 
     return total
+
 
 
 
