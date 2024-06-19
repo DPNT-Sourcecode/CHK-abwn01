7de4527a9ae8bc9295d41fe285b1c71751c6a87b
sbx_user1051 <sbx_user1051@169.254.88.21>
2024-06-19 17:52:22 +0000

    Wed Jun 19 17:52:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
+from collections import Counter
+
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+offers = {
+    "A": (3, 130),
+    "B": (2, 45)
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if not isinstance(skus, str):
+        return -1
+
+    total = 0
+    cart = Counter(skus)
+
